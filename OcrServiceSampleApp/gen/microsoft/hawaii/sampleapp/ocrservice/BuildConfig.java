38d2555f51579d9b5ab86417fb5fa2597a7f0602
mschleifer <matthewschleifer@gmail.com>
2013-04-01 21:14:46 -0500

    fixed issue of image resetting on screen orientation rotate

--- OcrServiceSampleApp/gen/microsoft/hawaii/sampleapp/ocrservice/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package microsoft.hawaii.sampleapp.ocrservice;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
