7bb1bf53473b3f18358bb65eaaed02ae36f3aaa4
Matt <matthewschleifer@gmail.com>
2013-05-06 15:06:52 -0500

    moved OCRTask class to it's own file

--- MainActivity/src/cs407/snapendar/main/MainActivity.java
+++ MainActivity/src/cs407/snapendar/main/MainActivity.java
@@ -1,45 +1,28 @@
 package cs407.snapendar.main;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
-import microsoft.hawaii.hawaiiClientLibraryBase.Listeners.OnCompleteListener;
-import microsoft.hawaii.hawaiiClientLibraryBase.Util.Utility;
 import cs407.snapendar.main.R;
-import microsoft.hawaii.sdk.OcrService.OcrService;
-import microsoft.hawaii.sdk.OcrService.ServiceContracts.OcrServiceResult;
-import microsoft.hawaii.sdk.OcrService.ServiceContracts.OcrText;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.text.format.Time;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.*;
-
-import com.mdimension.jchronic.Chronic;
-import com.mdimension.jchronic.utils.Span;
 
 //TODO: Make it so that when the screen is rotated the activity doesn't reset
 //TODO: Need to save data across the "Activity Lifetime Cycle" i.e onCreate/onDestroy
@@ -213,115 +196,7 @@ public class MainActivity extends HawaiiBaseAuthActivity {
 
 	private void onRecognizeButtonClick(View v) {
 		this.recognizeButton.setEnabled(false);
-
-		//final MainActivity thisActivity = this;
-		class OcrTask extends AsyncTask<Void, Integer, AlertDialog.Builder> {
-			private OcrServiceResult serviceResult;
-			
-			public final MainActivity thisActivity;
-
-		    public OcrTask(MainActivity a)
-		    {
-		        thisActivity = a;
-		    }
-
-			protected AlertDialog.Builder doInBackground(Void... listTypes) {
-				// get image from ImageView
-				Bitmap image = ((BitmapDrawable) imageView.getDrawable())
-						.getBitmap();
-				byte[] imageBytes = getImageByte(image);
-				if(imageBytes.length >= 1572864) {
-					
-					return new AlertDialog.Builder(thisActivity)
-				    .setTitle("Size Warning")
-				    .setMessage("Your image is " + (double)imageBytes.length + 
-							"B in size. This may cause unexpected errors.")
-				    .setPositiveButton("Continue", new OnClickListener() {
-				        public void onClick(DialogInterface dialog, int which) { 
-				            // continue with delete
-				        }
-				     })
-				    .setNegativeButton("Cancel", new OnClickListener() {
-				        public void onClick(DialogInterface dialog, int which) { 
-				            // do nothing
-				        }
-				     });
-					
-					/*return dialogToShow(
-							"Your image is " + 
-							(double)imageBytes.length + 
-							"B in size. This may cause unexpected errors.",
-							new Exception());*/
-				}
-				try {
-					OcrService.recognizeImage(thisActivity.getBaseApplication()
-							.getClientIdentity(), imageBytes,
-							new OnCompleteListener<OcrServiceResult>() {
-								public void done(OcrServiceResult result) {
-									serviceResult = result;
-									
-								}
-							}, null);
-				} catch (Exception exception) {
-					return dialogToShow("Couldn't execute the ocr operation",
-							exception);
-				}
-
-				if (serviceResult.getStatus() != microsoft.hawaii.hawaiiClientLibraryBase.Status.Success) {
-					return dialogToShow(
-							"Error when using ocr service to recognize the specified image",
-							serviceResult.getException());
-				}
-
-				return null;
-			}
-
-			protected void onPreExecute() {
-				resultContainer.setVisibility(View.GONE);
-				progressBar.setVisibility(View.VISIBLE);
-				ocrResultView.setText("");
-				imageView.setVisibility(View.GONE);
-			}
-
-			protected void onPostExecute(AlertDialog.Builder dialogBuilder) {
-				if (dialogBuilder != null) {
-					thisActivity.showErrorMessage(dialogBuilder);
-				} else {
-					resultContainer.setVisibility(View.VISIBLE);
-					List<OcrText> resultList = serviceResult.getOcrTexts();
-					if (resultList.size() > 0) {
-						String ocrResult = resultList.get(0).getText();
-						if (!Utility.isStringNullOrEmpty(ocrResult)) {
-							ocrResultView.setText(ocrResult);
-							
-							Span chronicDate = Chronic.parse(ocrResult);
-							if(chronicDate != null) {
-								chronicCalendar = chronicDate.getBeginCalendar();
-								ocrResultView.setText("Year: " + chronicCalendar.get(Calendar.YEAR) + 
-													  "\nMonth: " + (chronicCalendar.get(Calendar.MONTH)+1) +
-													  "\nDay: " + chronicCalendar.get(Calendar.DAY_OF_MONTH));
-								recognizeButton.setVisibility(View.GONE);
-								setEventButton.setVisibility(View.VISIBLE);
-							}
-							else {
-								thisActivity.showErrorMessage("Couldn't parse a date from specified image",
-										null);
-							}
-							
-						} else {
-							thisActivity.showErrorMessage("Couldn't recognize the specified image", 
-									null);
-						}
-					}
-				}
-
-				recognizeButton.setEnabled(true);
-				progressBar.setVisibility(View.GONE);
-				imageView.setVisibility(View.VISIBLE);
-				currentOcrTask = null;
-			}
-		}
-
+		
 		currentOcrTask = new OcrTask(this);
 		currentOcrTask.execute();
 	}
@@ -330,17 +205,5 @@ public class MainActivity extends HawaiiBaseAuthActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-	}
-
-	/**
-	 * Converts a Bitmap to a byte[]. Need to use in order to send our image
-	 * to the Project Hawaii OCR service
-	 * @param targetBitmap The Bitmap to convert
-	 * @return A byte[] representation of the Bitmap
-	 */
-	private static byte[] getImageByte(Bitmap targetBitmap) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		targetBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-		return baos.toByteArray();
 	}
 }
