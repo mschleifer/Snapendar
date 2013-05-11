e7899e188dfced728dfa61511385fa0fda61f73e
Alex Sohail <elementrat@gmail.com>
2013-05-11 11:22:48 -0500

    Re-added test-write to storage for demo purposes

--- MainActivity/src/cs407/snapendar/main/MainActivity.java
+++ MainActivity/src/cs407/snapendar/main/MainActivity.java
@@ -89,6 +89,7 @@ public class MainActivity extends HawaiiBaseAuthActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		storage = new Storage();
+		storage.writeTestFile();
 		
 		boolean hasRunBefore = storage.snapendarDirectoryExisted();
 		if(!hasRunBefore){
